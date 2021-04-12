6dc0d875818c1e95b89b69a5fdefeb8a40c4afb3
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2021-04-12 07:10:43 -0500

    Working prototype

--- echo.py
+++ echo.py
@@ -1,3 +1,23 @@
-def echo(string):
-    string = string.replace('e', ' echo ')
-    return string
+PHONETIC_LIST = "alfa bravo charlie delta echo foxtrot golf hotel india juliet kilo lima mike november oscar papa quebec romeo sierra tango uniform victor whiskey xray yankee zulu".split()
+
+ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+
+def echo(input_string):
+    """Replace all letters in a string with their phonetic alphabet
+    words.
+    """
+    input_string = input_string.lower()
+    output_string = ""
+    for char in input_string: # so we don't .replace() more than once
+        for phonetic in PHONETIC_LIST:
+            if char == phonetic[0]:
+                output_string += phonetic + ' '
+        if char == ' ':
+            output_string += ', '
+        elif char not in ALPHABET:
+            output_string += char
+    # Tidy up.
+    output_string = output_string.replace(' ,', ',') # No sp before comma
+    output_string = output_string.replace(',,', ';')
+    output_string = output_string.capitalize()
+    return output_string
--- main.py
+++ main.py
@@ -3,3 +3,9 @@ from echo import echo
 S = 'The quick brown fox jumps over the lazy dogs'
 
 print(echo(S))
+print()
+print()
+print(echo(echo(S)))
+print()
+print()
+print(echo(echo('Hi')))
